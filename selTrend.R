624b60e693b39fc3376cc4bd1bc6b4661b8de1b1
Veeresh Belgur <viru.belgur@gmail.com>
2017-06-13 20:36:54 +0530

    Add files via upload

---
+++ selTrend.R
@@ -0,0 +1,22 @@
+library(RSelenium)
+library(wdman)
+remDr = remoteDriver(remoteServerAddr="localhost",port = 4444L,browserName = "chrome")
+remDr$open(silent=T)
+remDr$navigate("https://trends.google.com/trends/")
+
+webelm<-remDr$findElement("id","input-0")
+webelm$sendKeysToElement(list("logan",key='enter'))
+
+
+webelm45<-remDr$findElement("class","widget-actions-menu")
+webelm45$clickElement()
+
+webelm58<-remDr$findElement("class","csv-image")
+webelm58$clickElement()
+
+
+setwd('C:/Users/Viru/Downloads')
+
+mydata <- read.csv("multiTimeline.csv")
+
+matplot(mydata[, 1], type="l")
